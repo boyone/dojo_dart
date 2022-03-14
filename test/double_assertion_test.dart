6d84d390f85a5efee45a18312dd82bf1b0badc29
boyone <boyone.g@gmail.com>
2022-03-14 09:55:19 +0700

    Add double assertion test

---
+++ test/double_assertion_test.dart
@@ -0,0 +1,16 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('assert double and integer', () {
+    expect(1000.00, 1000);
+  });
+
+  test('assert double and String', () {
+    expect(1000.00.toString(), isNot(equals('1000.00')));
+    expect(1000.00.toString(), equals('1000.0'));
+  });
+
+  test('assert double(with fraction digits) and String', () {
+    expect(1000.00.toStringAsFixed(2), equals('1000.00'));
+  });
+}
